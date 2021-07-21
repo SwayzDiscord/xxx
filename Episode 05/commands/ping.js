7ea7b5bdc6fde098a137d40cb72f3f6ab669136d
Swayz <77362066+SwayzDiscord@users.noreply.github.com>
2021-07-21 16:59:41 -0400

    Delete Episode 05 directory

--- Episode 05/commands/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    commands: ['p', 'ping'],
-    description: 'Smple Ping Command', //Optional
-    callback: (message, args) => {
-        message.channel.send('Pong!')
-    }
-}
