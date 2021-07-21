dab3d1b3290b3b3f4f848ab6f5350dceab552408
Swayz <77362066+SwayzDiscord@users.noreply.github.com>
2021-07-21 17:02:41 -0400

    Delete Episode 21 directory

--- Episode 21/commands/Fun/youtube.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    commands: ['youtube', 'yt'], // You Can Keep Any Name
-    description: 'Sends YT Link', // Optional
-    callback: (message, args) => {
-        message.channel.send('https://youtube.com/techtipcyber')
-    }
